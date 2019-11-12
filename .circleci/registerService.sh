b1d88e8942cc71c2cefc745f0f59324ce6d93f9e
Mou-Chun Wang <mou.wang@g.ncu.edu.tw>
2019-11-12 15:57:15 +0800

    Trying.... to connect

---
+++ .circleci/registerService.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+aws ecs register-task-definition --cli-input-json file://taskDef.json
---
+++ .circleci/updateService.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws ecs update-service \
+    --cluster FargateStack-fargateserviceautoscalingD107CF93-10UIXIP3G84N5 \
+    --service FargateStack-nameserviceServiceE5769334-YH1WVQUWCKP9 \
+    --task-definition "jobHunter-Family:3"
