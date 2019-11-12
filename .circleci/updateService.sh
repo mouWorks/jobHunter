1d627a0f2a2729031f0ef40e078b31a1d53fe4e7
mouWorks <mou.wang@g.ncu.edu.tw>
2019-11-12 21:48:42 +0800

    try this again

--- .circleci/updateService.sh
+++ .circleci/updateService.sh
@@ -3,4 +3,4 @@
 aws ecs update-service \
     --cluster FargateStack-fargateserviceautoscalingD107CF93-10UIXIP3G84N5 \
     --service FargateStack-nameserviceServiceE5769334-YH1WVQUWCKP9 \
-    --task-definition "jobHunter-Family:"+REVISION
+    --task-definition "jobHunter-Family:${REVISION}"
