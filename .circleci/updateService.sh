e3cbcb16045bbdf49aef28e6729d11e2bd238749
Mou-Chun Wang <mou.wang@g.ncu.edu.tw>
2019-11-13 22:04:28 +0800

    rollback 1113

--- .circleci/updateService.sh
+++ .circleci/updateService.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
-
 export TARGET="jobHunter-Family:${REVISION}"
 
 echo $TARGET
 
 aws ecs update-service \
-    --cluster FargateStack-fargateserviceautoscalingD107CF93-10UIXIP3G84N5 \
-    --service FargateStack-nameserviceServiceE5769334-YH1WVQUWCKP9 \
+    --cluster FargateStack-fargateserviceautoscalingD107CF93-1L43D3RJL9411 \
+    --service FargateStack-nameserviceServiceE5769334-J159BD0XSLXY \
     --task-definition ${TARGET}
